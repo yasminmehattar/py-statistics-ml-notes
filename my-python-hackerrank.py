dbdda80cfb7b7a324cf1dc0c988841780755b309
manoharreddyporeddy <manoharporeddy@outlook.com>
2025-05-25 12:58:31 +0530

    commit

---
+++ my-python-hackerrank.py
@@ -0,0 +1,54 @@
+"""
+python training
+
+YES
+    - https://www.hackerrank.com/domains/tutorials/10-days-of-statistics
+    - https://leetcode.com/problemset/pandas/
+    - https://www.hackerrank.com/domains/ai
+
+PROBLEM SOLVING
+    - https://www.hackerrank.com/domains/tutorials/30-days-of-code
+    - https://www.hackerrank.com/domains/python
+
+job apply
+    - https://www.hackerrank.com/apply?roles=%5B%22Back-End+Developer+%28Python%29%22%2C%22Data+Analyst+%28Python%29%22%5D&experiences=%5B%220-3+years%22%5D
+
+"""
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print("Hello, World.")
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+print(input_string)
+
+# echo ym111111111111 | python my-python-hackerrank.py
+# echo hello | python my-python-hackerrank.py
+# echo hellowwwww | python my-python-hackerrank.py
+# echo py cour | python my-python-hackerrank.py
+
+
+i = 4
+d = 4.0
+s = "HackerRank "
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+my_i = int(input())
+my_d = float(input())
+my_s = str(input())
+
+print(i + my_i)
+print(d + my_d)
+print(s + my_s)
