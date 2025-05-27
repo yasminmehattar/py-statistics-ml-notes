3c1683c0aaf8054951ff1aa9e6c72d8c97bceec2
yasminmehattar <yasminmehattar@gmail.com>
2025-05-27 14:29:41 +0530

    commit

---
+++ my_python _practice.py
@@ -0,0 +1,28 @@
+# print ("=======for checking python version=========")
+# import sys
+
+# print(sys.version)
+# print()
+
+# print("==========for checking indentation========")
+
+# if 5 > 2:
+#  print("Five is greater than two!") 
+# if 5 > 2:
+#         print("Five is greater than two!")
+# print()
+# print("=====we have to use the same number of spaces in the same block of code==========")
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
+'''Python will take it as comment until it is assigned it to a variable'''
+"""same as with double quotes too"""
+print()
+print("======vairables=======")
+x=5
+y="yasmin"
+print(x, y, sep=", ")
+print()
+X="Good" #does not overwrite X and x is not same
+print(x,X,y, sep=",")
