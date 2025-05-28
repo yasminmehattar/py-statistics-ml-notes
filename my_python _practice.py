dc9bb8628689bd18f61ed3510ce187bcc325ab4d
yasminmehattar <yasminmehattar@gmail.com>
2025-05-28 15:59:59 +0530

    commit

--- my_python _practice.py
+++ my_python _practice.py
@@ -18,11 +18,34 @@
 
 '''Python will take it as comment until it is assigned it to a variable'''
 """same as with double quotes too"""
-print()
-print("======vairables=======")
-x=5
-y="yasmin"
-print(x, y, sep=", ")
-print()
-X="Good" #does not overwrite X and x is not same
-print(x,X,y, sep=",")
+# print()
+# print("======vairables=======")
+# x=5
+# y="yasmin"
+# print(x, y, sep=", ")
+# print()
+# X="Good" #does not overwrite X and x is not same
+# print(x,X,y, sep=",")
+# a=int("5")
+# print(a)
+# A=float(6)# by casting we can change the datatype
+# print("A")
+# print(A)
+# print(type(A))
+# print(type(a))
+# Myvar=45
+# print(Myvar)
+# d,f,g="orange", "grapes", "apple"
+# print(d,f,g)
+# d,f,g=["orange", "grapes", "apple"]
+# print(d,f,g)
+# d=f=g="apple"
+# print(d,f,g)
+# print(d+f+g)#we can use + operator also to combine for numbers it work as math operator
+# h=1
+# j=2
+# print(h+j)#we can use + operator also to combine for numbers it work as math operator
+# print('Hello', 'World')
+import random
+
+print(random.randrange(1, 10))
